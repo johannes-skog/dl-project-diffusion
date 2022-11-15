b3dd9cc4719a09c8303107aa0be587bdb13b6818
johannes <johannesskog89@gmail.com>
2022-11-15 20:07:19 +0100

    Cleanup of celebA diffusion

--- unet.py
+++ unet.py
@@ -20,6 +20,7 @@ class Unet(torch.nn.Module):
     def __init__(
         self,
         channels: List[int],
+        attention_channels: List[int],
         in_channels: int = 3,
         resnet_block_groups: int = 8,
         use_convnext: bool = True,
@@ -73,7 +74,8 @@ class Unet(torch.nn.Module):
                     [
                         _ConvBlock(_current_channels, channels_out, time_emb_dim=time_dim),
                         _ConvBlock(channels_out, channels_out, time_emb_dim=time_dim),
-                        Residual(GroupPreNormalizer(channels_out, ConvLinearAttention(channels_out))),
+                        Residual(GroupPreNormalizer(channels_out, ConvAttention(channels_out))) if (channels_out in attention_channels)
+                        else torch.nn.Identity(),
                         # Downsample
                         (
                             torch.nn.Conv2d(
@@ -92,7 +94,7 @@ class Unet(torch.nn.Module):
             _current_channels = channels_out
 
         self.mid_block1 = _ConvBlock(_current_channels, _current_channels, time_emb_dim=time_dim)
-        self.mid_attn = Residual(GroupPreNormalizer(_current_channels, ConvLinearAttention(_current_channels)))
+        self.mid_attn = Residual(GroupPreNormalizer(_current_channels, ConvAttention(_current_channels)))
         self.mid_block2 = _ConvBlock(_current_channels, _current_channels, time_emb_dim=time_dim)
 
         _channels = [x for x in (reversed(_channels))][1:]
@@ -106,7 +108,8 @@ class Unet(torch.nn.Module):
                     [
                         _ConvBlock(2 * _current_channels, channels_out, time_emb_dim=time_dim),
                         _ConvBlock(channels_out, channels_out, time_emb_dim=time_dim),
-                        Residual(GroupPreNormalizer(channels_out, ConvLinearAttention(channels_out))),
+                        Residual(GroupPreNormalizer(channels_out, ConvAttention(channels_out))) if (channels_out in attention_channels)
+                        else torch.nn.Identity(),
                         (
                             torch.nn.ConvTranspose2d(
                                 in_channels=channels_out,
@@ -179,94 +182,6 @@ class Unet(torch.nn.Module):
         y = self.final_conv(x)
 
         return y
-
-
-class Net(torch.nn.Module):
-
-    def __init__(
-        self,
-        channels: List[int],
-        in_channels: int = 3,
-        resnet_block_groups: int = 8,
-        use_convnext: bool = True,
-        convnext_mult: int = 2,
-        init_channel_mult: int = 32,
-    ):
-        super().__init__()
-
-        _channels = channels[:]
-
-        # determine dimensions
-        self._nchannels = in_channels
-
-        _current_channels = in_channels
-
-        self._init_conv = torch.nn.Conv2d(
-            in_channels=_current_channels,
-            out_channels=_current_channels * init_channel_mult,
-            kernel_size=7,
-            padding=3
-        )
-
-        _current_channels = _current_channels * init_channel_mult
-
-        if use_convnext:
-            _ConvBlock = partial(ConvNextBlock, mult=convnext_mult)
-        else:
-            _ConvBlock = partial(ResnetBlock, norm_groups=resnet_block_groups)
-
-        time_dim = _current_channels * 4
-
-        self._time_mlp = torch.nn.Sequential(
-            SinusoidalPositionEmbeddings(_current_channels),
-            torch.nn.Linear(_current_channels, time_dim),
-            torch.nn.GELU(),
-            torch.nn.Linear(time_dim, time_dim),
-        )
-
-        # layers
-        self._down_sampling_layers = torch.nn.ModuleList([])
-        self._up_sampling_layers = torch.nn.ModuleList([])
-
-        n_layers = len(channels)
-
-        for i, channels_out in enumerate(_channels):
-
-            self._down_sampling_layers.append(
-                torch.nn.ModuleList(
-                    [
-                        _ConvBlock(_current_channels, channels_out, time_emb_dim=time_dim),
-                        _ConvBlock(channels_out, channels_out, time_emb_dim=time_dim),
-                        Residual(GroupPreNormalizer(channels_out, ConvLinearAttention(channels_out))),
-                    ]
-                )
-            )
-
-            _current_channels = channels_out
-
-        self.final_conv = torch.nn.Sequential(
-            _ConvBlock(_current_channels, _current_channels),
-            torch.nn.Conv2d(_current_channels, in_channels, 1, bias=True)
-        )
-
-    def forward(self, x, time):
-
-        x = self._init_conv(x)
-
-        t = self._time_mlp(time) if self._time_mlp is not None else None
-
-        h = []
-
-        # downsample
-        for block1, block2, attn in self._down_sampling_layers:
-            x = block1(x, t)
-            x = block2(x, t)
-            x = attn(x)
-        y = self.final_conv(x)
-
-        return y
-
-
 
 
 from utils import ClassEmbeddings
